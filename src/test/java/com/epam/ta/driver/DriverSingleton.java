e65aa4bd966827e74dfd7820552a30e544c2bd81
Dzmitry Karneyenka <dzmitry_karneyenka@epam.com>
2018-06-12 17:42:19 +0300

    change driver singleton for running parallel tests with Grid node using GRID.xml

--- src/test/java/com/epam/ta/driver/DriverSingleton.java
+++ src/test/java/com/epam/ta/driver/DriverSingleton.java
@@ -1,8 +1,13 @@
 package com.epam.ta.driver;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class DriverSingleton {
@@ -11,13 +16,21 @@ public class DriverSingleton {
     private static final String WEBDRIVER_GECKO_DRIVER = "webdriver.gecko.driver";
     private static final String GECKODRIVER_GECKODRIVER_EXE_PATH = ".\\src\\test\\resources\\geckodriver.exe";
 
-    private DriverSingleton(){};
+    private DriverSingleton(){}
 
 
     public static WebDriver getDriver(){
         if (null == driver){
-            System.setProperty(WEBDRIVER_GECKO_DRIVER, GECKODRIVER_GECKODRIVER_EXE_PATH);
-            driver = new FirefoxDriver();
+            //System.setProperty(WEBDRIVER_GECKO_DRIVER, GECKODRIVER_GECKODRIVER_EXE_PATH);
+            //driver = new FirefoxDriver();
+            DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+            capabilities.setPlatform(Platform.WINDOWS);
+            try {
+                driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
             driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
             driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
             driver.manage().window().maximize();
